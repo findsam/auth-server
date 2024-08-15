4f2f92d8af6f2692e43b5a8c205ad0621b48c16d
sam <84742666+findsam@users.noreply.github.com>
2024-08-15 13:10:37 +0100

    setup project and mod directories

--- config/config.go
+++ config/config.go
@@ -10,14 +10,12 @@ var Envs = initConfig()
 
 func initConfig() t.Config {
 	return t.Config{
-		Env:               getEnv("ENV", "development"),
-		Port:              getEnv("PORT", "8080"),
-		MongoURI:          getEnv("MONGODB_URI", "mongodb://localhost:27017"),
-		PublicURL:         getEnv("PUBLIC_URL", "http://localhost:3000"),
-		JWTSecret:         getEnv("JWT_SECRET", "JWT secret is required"),
-		SendGridAPIKey:    getEnv("SENDGRID_API_KEY", "SendGrid API KEY is required"),
-		SendGridFromEmail: getEnv("SENDGRID_FROM_EMAIL", "SendGrid From email is required"),
-		APIKey:            getEnv("API_KEY", "API Key is required"),
+		Env:       getEnv("ENV", "development"),
+		Port:      getEnv("PORT", "8080"),
+		MongoURI:  getEnv("MONGODB_URI", "mongodb://localhost:27017"),
+		PublicURL: getEnv("PUBLIC_URL", "http://localhost:3000"),
+		JWTSecret: getEnv("JWT_SECRET", "JWT secret is required"),
+		APIKey:    getEnv("API_KEY", "API Key is required"),
 	}
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module food-server
+module github.com/findsam/food-server
 
 go 1.22.5
